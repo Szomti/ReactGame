11204e60bd5a14beb88bc5936cc363ac4bfb4de5
Mateusz <mati.cibor1285@gmail.com>
2023-03-05 20:40:56 +0100

    Cleaned up a bit

--- src/App.js
+++ src/App.js
@@ -4,71 +4,53 @@ import Player from './tiles/Player.js';
 import RockTile from './tiles/RockTile.js';
 import SpaceTile from  './tiles/SpaceTile.js';
 import WallTile from './tiles/WallTile.js';
+import Maps from './maps/Maps.js';
 
 
 class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      
+      map: Maps.mapA
     }
   }
   
   render() {
+    document.addEventListener('keyup', (event) => {
+      if(event.code === 'KeyW'){
+        let temp = this.state.map;
+        temp[0][0] = 'S';
+        this.setState({
+          map: temp
+        });
+      }
+    }, false);
+    
     return (
       <div class='board'>
-        {CreateMap()}
+        {this.createMap()}
       </div>
     );
   }
-};
 
-// document.addEventListener('keyup', (event) => {
-//   var name = event.key;
-//   var code = event.code;
-// }, false);
-
-function CreateMap() {
-  return map.map(row => {
-    return row.map(tile => {
-      switch(tile){
-        case ' ':
-          return <WallTile/>;
-        case 'S':
-          return <SpaceTile/>;
-        case 'R':
-          return <RockTile/>;
-        case 'P':
-          return <Player/>;
-        default:
-          return <WallTile/>;
-        }
+  createMap() {
+    return this.state.map.map(row => {
+      return row.map(tile => {
+        switch(tile){
+          case ' ':
+            return <WallTile/>;
+          case 'S':
+            return <SpaceTile/>;
+          case 'R':
+            return <RockTile/>;
+          case 'P':
+            return <Player/>;
+          default:
+            return <WallTile/>;
+          }
+        });
       });
-    });
-  }
-  
-  const map = [
-    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', ' ', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', ' '],
-    [' ', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', ' ', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', ' '],
-    [' ', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', ' ', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', ' '],
-    [' ', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', ' ', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', ' '],
-  [' ', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', ' ', ' ', ' ', 'S', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-  [' ', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'R', 'S', 'S', 'S', 'S', 'S', 'S', ' '],
-  [' ', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', ' '],
-  [' ', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'P', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', ' '],
-  [' ', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', ' '],
-  [' ', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', ' '],
-  [' ', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', ' '],
-  [' ', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', ' '],
-  [' ', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', ' '],
-  [' ', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', ' '],
-  [' ', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', ' '],
-  [' ', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', ' '],
-  [' ', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', ' '],
-  [' ', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', ' '],
-  [' ', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', ' '],
-  [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-]
+    }
+};
 
 export default Board;
